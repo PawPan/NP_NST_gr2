a67e9fc73d688e9438a0d851dd8f821ed469039c
Paweł Pankau <77025204+PawPan@users.noreply.github.com>
2021-01-07 18:32:19 +0100

    Update Silnia.cpp

--- Silnia.cpp
+++ Silnia.cpp
@@ -30,7 +30,7 @@ int main(int argc, char** argv) {
 
 	zmiennaGlobalnaInt = silInt(a);
 	globLong = silLong(b);
-	globDoub = silDouble(d);
+	globDoub = silDouble(d-2);
 
 	cout<<n<<"!(int) = "<<zmiennaGlobalnaInt<<endl;
 	cout<<n<<"!(long) = "<<globLong<<endl;
